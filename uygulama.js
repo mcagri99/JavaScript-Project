f224359afe20a7d3fabc7d14a3247cbfdfc310f3
mcagri99 <mcagriozturk@outlook.com>
2022-05-23 23:12:19 +0300

    done

---
+++ uygulama.js
@@ -0,0 +1,3 @@
+ document.getElementById('on').addEventListener("click", function() { document.getElementById('lamba').src ='lamba_acik.jpg' });
+
+ document.getElementById('off').addEventListener("click", function() { document.getElementById('lamba').src ='lamba_kapali.jpg' });
